38636c4adf91f901ab15c4dd92a2a747a18af1c7
imbadwithnamestoo <91441184+imbadwithnamestoo@users.noreply.github.com>
2026-05-03 19:38:50 -0400

    Update main.cpp
    Version 1.1

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <chrono>
 #include <fstream>
 #include <cstdlib>
+#include <cmath>
 
 #define wait(x) std::this_thread::sleep_for(std::chrono::milliseconds(x))
 
@@ -44,6 +45,7 @@ int main() {
 
 	int totalWins = 0;
 	int totalLoss = 0;
+	int totalPoints = 100;
 
 	std::string playerName;
 
@@ -55,13 +57,15 @@ int main() {
 
 		std::ofstream outFile("save.txt");
 		if (outFile.is_open()) {
-			outFile << playerName << std::endl;
-			outFile << 0 << std::endl;
-			outFile << 0;
+			outFile << playerName << std::endl; // username
+			outFile << 100 << std::endl; // points
+			outFile << 0 << std::endl; // wins
+			outFile << 0; // losses
 			outFile.close();
 		}
 	} else {
 		checkFile >> playerName;
+		checkFile >> totalPoints;
 		checkFile >> totalWins;
 		checkFile >> totalLoss;
 		checkFile.close();
@@ -69,7 +73,7 @@ int main() {
 		wait(350);
 	}
 
-	std::cout << "\033[1;37mMAIN MENU\033[0m \033[33m//\033[0m \033[32m" << playerName << "\033[0m" << std::endl;
+	std::cout << "\033[1;37mSUPER GAMBLING\033[0m \033[33m//\033[0m \033[36mV1.1\033[0m \033[33m//\033[0m \033[32m" << playerName << "\033[0m" << std::endl;
     std::cout << "------------------------------------" << std::endl;
     
     std::cout << "\033[32m[1]\033[0m Start Game" << std::endl;
@@ -82,11 +86,11 @@ int main() {
 
 	switch (menuChoice) {
     case 2:
-		
         clear();
         std::cout << "\033[1;37mPLAYER STATS\033[0m \033[33m//\033[0m \033[32m" << playerName << "\033[0m" << std::endl;
         std::cout << "------------------------------------" << std::endl;
 
+		std::cout << "Points: \033[33m" << totalPoints << "\033[0m" << std::endl;
         std::cout << "Wins:   \033[1;32m" << totalWins << "\033[0m" << std::endl;
         std::cout << "Losses: \033[1;31m" << totalLoss << "\033[0m" << std::endl;
 		std::cout << "W/L:    \033[1;36m" << (totalLoss == 0 ? (float)totalWins : (float)totalWins / totalLoss) << "\033[0m" << std::endl;
@@ -105,14 +109,18 @@ int main() {
         exit(0);
         break;
 }
-
-
 	do {
 		std::srand(std::time(0));
 		int secretNum = std::rand() % 10 + 1;
 		int guess = 0;
 		int lives = 5;
 		int difficulty = 0;
+		int wager;
+		int minWager = totalPoints * 0.15;
+
+		if (minWager <= 0) {
+			minWager = totalPoints;
+		}
 
 		std::string status = "I'm thinking of a number between 1 and 10.";
 
@@ -120,7 +128,7 @@ int main() {
 		std::cout << "\033[1;37mDIFFICULTY\033[0m \033[33m//\033[0m \033[32m" << playerName << "\033[0m" << std::endl;
 		std::cout << "------------------------------------" << std::endl;
 
-		std::cout << "\033[32m1.\033[0m Easy   (10 lives)" << std::endl;
+		std::cout << "\033[32m1.\033[0m Easy   (7 lives)" << std::endl;
 		std::cout << "\033[33m2.\033[0m Normal (5 lives)" << std::endl;
 		std::cout << "\033[31m3.\033[0m Hard   (3 lives)" << std::endl;
 		std::cout << "\033[1;37m-1.\033[0m Exit to menu" << std::endl;
@@ -132,14 +140,33 @@ int main() {
 
 		switch (difficulty) {
 		case 1:
-			lives = 10;
+			lives = 7;
+			wager = 0;
 			status = "Difficulty: \033[32mEasy\033[0m";
+			std::cout << "Easy mode selected! No wagering in this mode." << std::endl;
+			wait(1500);
 			break;
 		case 2:
 			lives = 5;
 			status = "Difficulty: \033[33mNormal\033[0m";
+			std::cout << "\033[1;37mMinimum: \033[0m" << minWager << "\033[1;37m. Maximum: \033[0m" << totalPoints << std::endl;
+ 			std::cout << "\033[1;37mWager:\033[0m ";
+			std::cin >> wager;
+			if (wager > totalPoints || wager < minWager) {
+				std::cout << "Invalid wager amount, going all-in!" << std::endl;
+				wait(1500);
+				wager = totalPoints;
+			}
 			break;
 		case 3:
+			std::cout << "\033[1;37mMinimum: \033[0m" << minWager << "\033[1;37m. Maximum: \033[0m" << totalPoints << std::endl;
+ 			std::cout << "\033[1;37mWager:\033[0m ";
+			std::cin >> wager;
+			if (wager > totalPoints || wager < minWager) {
+				std::cout << "Invalid wager amount, going all-in!" << std::endl;
+				wait(1500);
+				wager = totalPoints;
+			}
 			status = "Difficulty: \033[31mHard\033[0m";
 			lives = 3;
 			break;
@@ -147,7 +174,14 @@ int main() {
 			status = "Difficulty: \033[33mExtreme\033[0m";
 			lives = 5;
 			std::cout << "wow you found the secret difficulty!" << std::endl;
-			wait(1500);
+			std::cout << "\033[1;37mMinimum: \033[0m" << minWager << "\033[1;37m. Maximum: \033[0m" << totalPoints << std::endl;
+ 			std::cout << "\033[1;37mWager:\033[0m ";
+			std::cin >> wager;
+			if (wager > totalPoints || wager < minWager) {
+				std::cout << "Invalid wager amount, going all-in!" << std::endl;
+				wait(1500);
+				wager = totalPoints;
+			}
 			break;
 		case -1:
 			main();
@@ -175,7 +209,7 @@ int main() {
 				if (checkLives(lives)) {
 					if (guess > secretNum) {
 						if (difficulty == 4) {
-							status = "Lower! Range removed and number moved 3";
+							status = "Lower! Range removed and number shifted 3";
 							secretNum = newNum(secretNum, guess);
 						} else {
 							status = "\033[31mLower!\033[0m";
@@ -183,7 +217,7 @@ int main() {
 					}
 					if (guess < secretNum) {
 						if (difficulty == 4) {
-							status = "Higher! Range removed and number moved 3";
+							status = "Higher! Range removed and number shifted 3";
 							secretNum = newNum(secretNum, guess);
 						} else {
 							status = "\033[31mHigher!\033[0m";
@@ -192,16 +226,29 @@ int main() {
 				} else {
 					totalLoss++;
 					clear();
+
+					totalPoints -= wager;
+
+					if (totalPoints <= 0) {
+						std::cout << "You've gone bankrupt! Your save file has been deleted." << std::endl;
+						remove("save.txt");
+						wait(1000);
+						exit(0);
+					}
+
 					std::ofstream outFile3("save.txt");
 					if (outFile3.is_open()) {
 						std::cout << "Saving changes..." << std::endl;
 						outFile3 << playerName << std::endl;
+						outFile3 << totalPoints << std::endl;
 						outFile3 << totalWins << std::endl;
 						outFile3 << totalLoss;
 						wait(400);
 						outFile3.close();
 					}
+					std::cout << "You lost " << wager << " points!" << std::endl;
 					std::cout << "\033[31mGame over!\033[0m The number was " << secretNum << "." << std::endl;
+
 					std::cout << "Play again? (y/n): " << std::endl;
 					std::cin >> choice;
 				}
@@ -210,10 +257,28 @@ int main() {
 			if (guess == secretNum) {
 				totalWins++;
 				clear();
+				switch (difficulty) {
+					case 1: 
+						std::cout << "Easy mode. No points awarded." << std::endl;
+						break;
+					case 2:
+						std::cout << "Awarded " << std::round(wager * 1.2) << " points!" << std::endl;
+						totalPoints += std::round(wager * 0.2);
+						break;
+					case 3:
+						std::cout << "Awarded " << std::round(wager * 1.55) << " points!" << std::endl;
+						totalPoints += std::round(wager * 0.55);
+						break;
+					case 4:
+						std::cout << "Awarded " << std::round(wager * 2) << " points!" << std::endl;
+						totalPoints += std::round(wager * 2);
+						break;
+				}
 				std::ofstream outFile2("save.txt");
 				if (outFile2.is_open()) {
 					std::cout << "Saving changes..." << std::endl;
 					outFile2 << playerName << std::endl;
+					outFile2 << totalPoints << std::endl;
 					outFile2 << totalWins << std::endl;
 					outFile2 << totalLoss;
 					wait(400);
@@ -221,6 +286,7 @@ int main() {
 				}
 
 				std::cout << "\033[32mYou won the game with " << lives << " lives!\033[0m" << std::endl;
+				
 				std::cout << "Play again? (y/n): " << std::endl;
 				std::cin >> choice;
 			}
